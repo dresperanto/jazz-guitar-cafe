e84c66be6d0a967df31f950a1a26021cb7b8db3b
Will Rainbow <will@muzemaster.com>
2018-09-24 13:49:52 -0400

    Create and Read setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import fire from "./fire";
+import DatePicker from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
+      startDate: '',
       title: '',
       url: '',
+      description: '',
       allData: []
     };
   }
@@ -25,11 +35,13 @@ class App extends React.Component {
     });
     db.collection('lessons').add({
       title: this.state.title,
-      url: this.state.url
+      url: this.state.url,
+      description: this.state.description
     });
     this.setState({
       title: '',
-      url: ''
+      url: '',
+      description: ''
     });
   };
 
@@ -56,21 +68,47 @@ class App extends React.Component {
       })
   }
 
+  handleChange(date) {
+    this.setState({ startDate: date })
+  }
+
   render() {
 
     var listOfData = this.state.allData.map((val, i) => {
       var title = val.title
+      var description = val.description
       var url = val.url
+      var key = val.id
       return (
-        <li key={i}>{title} <a href={url}>{url}</a></li>
+        <div key={key} class="ui middle aligned divided list">
+          <div className="item">
+            <div className="right floated content">
+              <div className="ui button">Edit</div>
+            </div>
+            <img className="ui avatar image" src="/images/lena.png" />
+            <div className="content">
+              {title} {url}<br />
+            </div>
+          </div>
+        </div>
+
+
+        // <li key={i}>{title} <a href={url}>{url}</a><br /></li>
       )
     })
 
     return (
-      <div class="ui raised very padded text container segment">
+      <div className="ui raised very padded text container segment" style={{ marginTop: "30px" }}>
 
         <h2>Add an Instructor</h2>
         <form onSubmit={this.addData} className="ui form">
+          <DatePicker
+            name="startDate"
+            selected={this.state.startDate}
+            onChange={this.handleChange}
+            placeholderText="Click to select a date"
+            value={this.state.startDate}
+          />
           <div className="field">
             <label>Title</label>
             <input
@@ -81,6 +119,7 @@ class App extends React.Component {
               value={this.state.title}
             />
           </div>
+
           <div className="field">
             <label>URL</label>
             <input
@@ -92,35 +131,26 @@ class App extends React.Component {
             />
           </div>
 
+          <div className="field">
+            <label>Description</label>
+            <input
+              type="text"
+              name="description"
+              placeholder="Title"
+              onChange={this.updateInput}
+              value={this.state.description}
+            />
+          </div>
+
           <button className="ui button" type="submit">Submit</button>
         </form>
 
-        {/* <form onSubmit={this.addData}>
-          <input
-            type="text"
-            name="name"
-            placeholder="Input your name..."
-            onChange={this.updateInput}
-            value={this.state.name}
-          />
-          <br />
-          <input
-            type="number"
-            name="age"
-            placeholder="Input your age..."
-            onChange={this.updateInput}
-            value={this.state.age}
-          />
-          <br />
-          <button type="submit">Submit</button>
-        </form> */}
-
-        <h4 class="ui horizontal divider header">
-          <i class="tag icon"></i>
+        <h4 className="ui horizontal divider header">
+          <i className="tag icon"></i>
           Lesson Providers
         </h4>
 
-        <button onClick={this.getData} class="positive ui button">List lesson providers</button>
+        <button onClick={this.getData} className="positive ui button">List lesson providers</button>
 
         <ul>{listOfData}</ul>
 
---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+require('firebase/firestore')
+
+var config = {
+  apiKey: "AIzaSyD-XCn45EccUDhkMKkEF29w-ULZ5XTzFyA",
+  authDomain: "muzemaster-lessons.firebaseapp.com",
+  databaseURL: "https://muzemaster-lessons.firebaseio.com",
+  projectId: "muzemaster-lessons",
+  storageBucket: "",
+  messagingSenderId: "1010208036989"
+};
+firebase.initializeApp(config);
+
+export const db = firebase.firestore()
